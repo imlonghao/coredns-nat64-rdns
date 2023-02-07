e434017bfb2919de78e426864839ff572caaf2f5
imlonghao <git@imlonghao.com>
2023-02-07 23:48:25 +0800

    feat: add bind and dns64

--- cmd/coredns.go
+++ cmd/coredns.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	_ "git.esd.cc/imlonghao/coredns-nat64-rdns"
+	_ "github.com/coredns/coredns/plugin/bind"
+	_ "github.com/coredns/coredns/plugin/dns64"
 
 	"github.com/coredns/coredns/core/dnsserver"
 	"github.com/coredns/coredns/coremain"
 )
 
 var directives = []string{
+	"bind",
+	"dns64",
 	"nat64-rdns",
 }
 
